13ece12c77b79919f3825302199678cee44a18bc
Skywalker91440 <stevenmsongole96@gmail.com>
2026-05-25 12:38:43 -0400

    Enable DEBUG for testing

--- sis_project/settings.py
+++ sis_project/settings.py
@@ -13,7 +13,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-2xq8p9v4m7k3w6n9t2r5y8u1i4o7p0l9k3j6m2n5b8v1c4x7z0a3s6d9f2g5h8'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*', '.railway.app', '.up.railway.app']
 
